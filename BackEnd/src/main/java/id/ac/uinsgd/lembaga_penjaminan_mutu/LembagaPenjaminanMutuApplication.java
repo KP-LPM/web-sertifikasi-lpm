dfd7d29227e3fcddea95fc0fe79b5506cfb43442
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2026-07-08 11:08:47 +0700

    feat: instalations springboot on backend

---
+++ BackEnd/src/main/java/id/ac/uinsgd/lembaga_penjaminan_mutu/LembagaPenjaminanMutuApplication.java
@@ -0,0 +1,13 @@
+package id.ac.uinsgd.lembaga_penjaminan_mutu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembagaPenjaminanMutuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembagaPenjaminanMutuApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/id/ac/uinsgd/lembaga_penjaminan_mutu/LembagaPenjaminanMutuApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.uinsgd.lembaga_penjaminan_mutu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembagaPenjaminanMutuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
